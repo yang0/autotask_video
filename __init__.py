8935d3cbb1192ea4201619ac0690aa9acc879a8e
yang0 <yang0@126.com>
2025-04-22 14:34:40 +0800

    添加视频切片中断机制

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from .cutVideo import *
 from .randomConcat import *
 from .muteVideo import *
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 GIT_URL = "https://github.com/yang0/autotask_video.git"
 NAME = "视频编辑相关功能"
 DESCRIPTION = "视频编辑相关功能"
